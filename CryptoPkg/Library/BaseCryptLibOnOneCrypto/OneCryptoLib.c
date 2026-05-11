6a3659c5df6709806df9c49a589d0acfe108d330
Doug Flick <dougflick@microsoft.com>
2026-05-11 13:21:58 -0700

    reverting changes for testing

--- CryptoPkg/Library/BaseCryptLibOnOneCrypto/OneCryptoLib.c
+++ CryptoPkg/Library/BaseCryptLibOnOneCrypto/OneCryptoLib.c
@@ -93,7 +93,7 @@ GetAndValidateCryptoProtocol (
   IN UINT16       MinMinor
   )
 {
-  DEBUG ((DEBUG_VERBOSE, "[%a] Calling crypto service: %a\n", gEfiCallerBaseName, FunctionName));
+  DEBUG ((DEBUG_ERROR, "[%a] Calling crypto service: %a\n", gEfiCallerBaseName, FunctionName));
 
   if (gCryptoProtocol == NULL) {
     DEBUG ((DEBUG_ERROR, "[%a] Crypto Protocol is NULL\n", gEfiCallerBaseName));
@@ -4457,39 +4457,7 @@ X509ConstructCertificateStackV (
   IN      VA_LIST  Args
   )
 {
-  ONE_CRYPTO_PROTOCOL  *CryptoServices;
-  UINT8                *Cert;
-  UINTN                CertSize;
-  BOOLEAN              Status;
-
-  if (X509Stack == NULL) {
-    return FALSE;
-  }
-
-  CryptoServices = GetAndValidateCryptoProtocol ("X509ConstructCertificateStack", 1, 0);
-  if ((CryptoServices == NULL) || (CryptoServices->X509ConstructCertificateStack == NULL)) {
-    CryptoServiceNotAvailable ("X509ConstructCertificateStack");
-    return FALSE;
-  }
-
-  Status = TRUE;
-  Cert   = VA_ARG (Args, UINT8 *);
-  while (Cert != NULL) {
-    CertSize = VA_ARG (Args, UINTN);
-    if (CertSize == 0) {
-      Status = FALSE;
-      break;
-    }
-
-    Status = CryptoServices->X509ConstructCertificateStack (X509Stack, Cert, CertSize, NULL);
-    if (!Status) {
-      break;
-    }
-
-    Cert = VA_ARG (Args, UINT8 *);
-  }
-
-  return Status;
+  CALL_CRYPTO_SERVICE (X509ConstructCertificateStackV, (X509Stack, Args), FALSE, 1, 0);
 }
 
 /**
